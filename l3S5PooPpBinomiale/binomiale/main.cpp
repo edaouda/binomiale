4544e82040cab6f9a402a5e8ebb26ad8cbff8165
edaouda <88290755+edaouda@users.noreply.github.com>
2021-08-20 18:38:15 +0200

    Add files via upload
    init commit

---
+++ l3S5PooPpBinomiale/binomiale/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <fstream>
+#include <string>
+
+float abs_function (const float & number) {
+    if (number < 0)
+        return -number;
+    return number;
+}
+
+void binomiale (const float & n_max, const float & p_max, const std::string & filename_data) {
+ float E_test, E_simple, ecart_E;
+ std::ofstream data_file;
+ data_file.open (filename_data);
+ data_file << "n,p,ecart_E\n";
+ std::cout << "n,p,ecart_E\n";
+
+ for (float n = 1; n <= n_max; ++n) {
+  for (float p = 0.1; p <= p_max; p = p + 0.1) {
+   // calcul de E_test
+   float succes_bernoulli;
+   E_test = 0;
+   for (float indice_bernoulli = 0; indice_bernoulli < n; ++indice_bernoulli) {
+    succes_bernoulli = (rand() % 100) + 1;
+    if (succes_bernoulli <= (p * 100))
+     ++E_test;
+   }
+
+   // calcul de E_simple
+   E_simple = n * p;
+
+   // calcul de ecart_E
+   ecart_E = abs_function(E_simple-E_test);
+
+   // affichage des resultat pour un n et p données
+   /*//
+   std::cout << std::endl << "n: " << n << ", p:" << p << std::endl;
+   std::cout << "E_test: " << E_test << std::endl;
+   std::cout << "E_simple: " << E_simple << std::endl;
+   std::cout << "ecart_E: " << ecart_E << std::endl;
+   //*/
+
+   // save data
+   //data_file << std::to_string(n) + std::to_string(p) + std::to_string(ecart_E) + "\n";
+   std::cout << std::to_string(E_test) + "," + std::to_string(E_simple) + "," + std::to_string(ecart_E) + "\n";
+   std::cout << std::to_string(n) + "," + std::to_string(p) + "," + std::to_string(ecart_E) + "\n\n";
+  }
+ }
+
+ data_file.close();
+}
+
+int main () {
+ srand(time(NULL));
+
+ float n_max, p_max;
+ n_max = 100;
+ p_max = 1;
+ std::string filename_data = "data_binomiale.csv";
+ binomiale(n_max, p_max, filename_data);
+}
+
+// utiliser https://courspython.com/visualisation-couleur.html
+// colorbar()
---
+++ l3S5PooPpBinomiale/readme.md.txt
@@ -0,0 +1,5 @@
+Ce projet utilise des notions de probabilité.
+Cela contiste à observer l'écart entre l'ésperence théorique (E=n*p) et réelle d'une loi binomiale selon n et p.
+n varie de 1 à 100  et p de 0.1 à 0.9.
+
+On observe que cette écart est souvent haute lorsque n est supérieure à 40.
